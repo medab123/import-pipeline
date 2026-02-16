8cc7a6a6bbad4efbaa0557afea7e47b5bcce195b
Mohammed Elabidi <mohammed.elabidi123@gmail.com>
2026-02-16 01:26:40 +0000

    add result save

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Enums\ToastNotificationVariant;
 use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 abstract class Controller
 {
+    use AuthorizesRequests;
+
     public function toast(string $message, ToastNotificationVariant $variant = ToastNotificationVariant::Default): self
     {
         $notifications = session()->get('toastNotifications', []);
