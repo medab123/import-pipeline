57bb2ed4b742aa9f8957be56fc92ebd312f83d2d
Mohammed Elabidi <mohammed.elabidi123@gmail.com>
2026-03-07 13:56:10 +0000

    Update import pipeline configuration and add test route for AI mapping
    - Modified `importPipelines.conf` to change the command path for the import pipelines worker.
    - Added a new test route in `web.php` to facilitate testing of the `ImportMapping` class, including fetching source and target fields and generating a prompt.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,24 @@
 <?php
 
+use App\Ai\Agents\ImportMapping;
+use Elaitech\Import\Models\ImportPipeline;
+use Elaitech\Import\Services\Pipeline\Services\FeedKeysService;
 use Illuminate\Support\Facades\Route;
 
 use Illuminate\Foundation\Application;
 use Inertia\Inertia;
+
+
+Route::get('test', function () {
+    $targetFields = \App\Models\TargetField::select('field')->pluck('field')->toArray();
+    $sourceFields = app(FeedKeysService::class)->getFeedKeys(ImportPipeline::find(6));
+    $model = ImportMapping::make();
+    $model->setSourceFields($sourceFields);
+    $model->setTargetFields($targetFields);
+    $res = $model->prompt('');
+    dd($res->toArray());
+});
+
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
